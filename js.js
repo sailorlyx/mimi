47d8e9271aa6780634257bdd47cd7756a74c3cda
Li <79230737+sailorlyx@users.noreply.github.com>
2022-03-25 11:20:53 +0100

    intento de slideimage

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
 function botones(nombre) {
     console.log("puta tu madre, " + nombre);
 }
+
+let slides = document.getElementsByClassName('mySlide');
+console.log('Sin esperar a que cargue: ' + slides.length);
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+   let slides = document.getElementsByClassName('mySlide');
+   console.log('Esperando a que cargue: ' + slides.length);
+});
